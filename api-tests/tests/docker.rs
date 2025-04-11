34c4edb04a0b1528fc5c11b9a60dce2ba99e38b2
Marc Schreiber <info@schrieveslaach.de>
2025-04-11 18:17:25 +0200

    Improve debugging experience

--- api-tests/tests/docker.rs
+++ api-tests/tests/docker.rs
@@ -3,15 +3,29 @@ mod container;
 
 use crate::container::{PREvant, Traefik};
 use container::{prevant_url, traefik_url};
-use testcontainers::runners::AsyncRunner;
+use log::Level;
+use testcontainers::{
+    core::logs::consumer::logging_consumer::LoggingConsumer, runners::AsyncRunner, ImageExt,
+};
 
 #[tokio::test]
 async fn should_deploy_nginx() {
+    env_logger::init();
     let traefik = Traefik::default()
+        .with_log_consumer(
+            LoggingConsumer::new()
+                .with_stdout_level(Level::Debug)
+                .with_stderr_level(Level::Debug),
+        )
         .start()
         .await
         .expect("container should be available");
     let prevant = PREvant::default()
+        .with_log_consumer(
+            LoggingConsumer::new()
+                .with_stdout_level(Level::Debug)
+                .with_stderr_level(Level::Debug),
+        )
         .start()
         .await
         .expect("container should be available");
@@ -21,11 +35,22 @@ async fn should_deploy_nginx() {
 
 #[tokio::test]
 async fn should_replicate_mariadb_with_replicated_env() {
+    env_logger::init();
     let _traefik = Traefik::default()
+        .with_log_consumer(
+            LoggingConsumer::new()
+                .with_stdout_level(Level::Debug)
+                .with_stderr_level(Level::Debug),
+        )
         .start()
         .await
         .expect("container should be available");
     let prevant = PREvant::default()
+        .with_log_consumer(
+            LoggingConsumer::new()
+                .with_stdout_level(Level::Debug)
+                .with_stderr_level(Level::Debug),
+        )
         .start()
         .await
         .expect("container should be available");
